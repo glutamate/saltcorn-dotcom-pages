2f1c24c0ede33f78ad97dd10ec1dfaec09028760
Tom Nielsen <tanielsen@gmail.com>
2020-05-12 22:17:38 +0100

    maybe

---
+++ pages.js
@@ -0,0 +1,11 @@
+const { textarea, text } = require("saltcorn-markup/tags");
+
+const root= {
+  above: [
+    { type: "hero",
+      caption: "Open source extensible no-code platform" 
+    }
+  ]
+}
+
+module.exports = { pages: {root} };
