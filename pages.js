cbf72a02e7b68810a689494651515922dcab4550
Tom Nielsen <tanielsen@gmail.com>
2020-06-15 21:24:47 +0100

    large follow btn

--- pages.js
+++ pages.js
@@ -80,7 +80,7 @@ const root = {
         besides: [          
           {
             type: "blank",
-            contents: div('For updates: <a href="https://twitter.com/saltcorns?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @saltcorns</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'),
+            contents: div('For updates: <a href="https://twitter.com/saltcorns?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large" data-show-count="false">Follow @saltcorns</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'),
            
           }
         ]
